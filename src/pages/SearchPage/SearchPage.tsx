6c541dd55caccc2e785d8a380a310a52b69b7209
andreyatx <mitrofanobandrei@gmail.com>
2023-03-23 17:25:21 +0300

    refactor: make constants file

--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -1,40 +1,30 @@
-import { FC } from "react";
+import { FC, FormEvent, useState } from "react";
 import { Outlet } from "react-router-dom";
 import { api } from "../../api";
 import { Select } from "../../components/Select";
 import styles from "./SearchPage.module.css";
 import SearchIcon from "../../assets/SearchIcon.svg";
-
-const HEADING = "Search for books";
-const CATEGORIES = "Categories";
-const SORTING = "Sorting by";
-
-const CATEGORIES_OPTIONS = [
-  "all",
-  "art",
-  "biography",
-  "computers",
-  "history",
-  "medical",
-  "poetry",
-];
-
-const SORTING_OPTIONS = ["relevance", "newest"];
+import { OPTIONS, SEARCH } from "./constants";
 
 export const SearchPage: FC = () => {
+  const [query, setQuery] = useState();
+
   api.get(
     `/books/v1/volumes?q=flowers+inauthor:keyes&key=${process.env.REACT_APP_API_KEY}`
   );
 
-  const submitHandler = (event: SubmitEvent) => {
+  const submitHandler = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     console.log("submitted");
   };
+
+  const handleInputChange = () => {};
+
   return (
     <div className={styles.searchContainer}>
-      <h1>{HEADING}</h1>
-      <form onSubmit={() => submitHandler}>
+      <h1>{SEARCH.HEADING}</h1>
+      <form onSubmit={submitHandler}>
         <div className={styles.searchField}>
           <input
             type="search"
@@ -44,12 +34,12 @@ export const SearchPage: FC = () => {
           />
           <button type="submit" className={styles.btnSearch}>
             <img src={SearchIcon} alt="search" className={styles.searchIcon} />
-            Найти
+            {SEARCH.FIND}
           </button>
         </div>
         <div className={styles.selectContainer}>
-          <Select name={CATEGORIES} options={CATEGORIES_OPTIONS} />
-          <Select name={SORTING} options={SORTING_OPTIONS} />
+          <Select name={SEARCH.CATEGORIES} options={OPTIONS.CATEGORIES} />
+          <Select name={SEARCH.SORTING} options={OPTIONS.SORTING} />
         </div>
       </form>
 
---
+++ src/pages/SearchPage/constants.ts
@@ -0,0 +1,19 @@
+export const SEARCH = {
+  HEADING: "Search for books",
+  CATEGORIES: "Categories",
+  SORTING: "Sorting by",
+  FIND: "Найти",
+};
+
+export const OPTIONS = {
+  CATEGORIES: [
+    "all",
+    "art",
+    "biography",
+    "computers",
+    "history",
+    "medical",
+    "poetry",
+  ],
+  SORTING: ["relevance", "newest"],
+};
